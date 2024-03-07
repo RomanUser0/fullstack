536b44f7500ec39356ba0110f5ed771e774f36ba
roman <zroman938@gmail.com>
2024-03-07 22:31:12 +0300

    upload

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -6,6 +6,11 @@ import styles from './profile.module.css'
 
 function Profile() {
 
+    useEffect(() => {
+        axios.post('api/users', { username: 'roman', password: 'password' }).then((res, req) => {
+            console.log(res)
+        })
+    }, [])
 
 
 
