08fc6f70327671582bf47520a1cab00cd9fe47f8
roman <zroman938@gmail.com>
2024-03-08 13:35:30 +0300

    проверка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
-//import { UsersController } from '../users/controller/users.controller';
-//import { UsersModule } from '../users/users.module';
-//import { UsersService } from '../users/service/users.service';
+import { UsersController } from '../users/controller/users.controller';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/service/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-//import entityes from '../entities';
+import entityes from '../entities';
 
 
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [UsersModule, TypeOrmModule.forRoot({
     type: 'mysql',
     host: 'localhost',
     port: 3306,
     username: 'roman',
     password: 'password',
     database: 'roman',
-    entities: [],
+    entities: entityes,
     autoLoadEntities: true,
   })],
-  controllers: [AppController, /*UsersController*/],
-  providers: [AppService, /*UsersService*/],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
