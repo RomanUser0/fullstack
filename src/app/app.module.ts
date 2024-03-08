6092ef02ea18fc588177ce6203280a9a52762f18
roman <zroman938@gmail.com>
2024-03-08 13:19:29 +0300

    проверка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './service/app.service';
 //import { UsersController } from '../users/controller/users.controller';
 //import { UsersModule } from '../users/users.module';
 //import { UsersService } from '../users/service/users.service';
-//import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 //import entityes from '../entities';
 
 
 
 
 @Module({
-  imports: [/*TypeOrmModule.forRoot({
+  imports: [TypeOrmModule.forRoot({
     type: 'mysql',
     host: 'localhost',
     port: 3306,
@@ -20,7 +20,7 @@ import { AppService } from './service/app.service';
     database: 'roman',
     entities: [],
     autoLoadEntities: true,
-  })*/],
+  })],
   controllers: [AppController, /*UsersController*/],
   providers: [AppService, /*UsersService*/],
 })
