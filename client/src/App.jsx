037fea2ff1347db615df104c5765fa59a299a08e
roman <zroman938@gmail.com>
2024-03-09 19:55:15 +0300

    проверка

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react'
 import styles from './app.module.css'
 import Content from './components/content/content'
 import Header from './components/header/header'
 import Navigation from './components/navigation/navigation'
+import Auth from './pages/auth/auth'
 
 
 function App() {
+
+  const [auth, setAuth] = useState(false)
 
 
   return (
     <div className={styles.appWrapper}>
       <Header />
-      <Navigation />
-      <Content />
+      {auth ?
+        <div>
+          <Navigation />
+          <Content />
+        </div>
+        :
+        <Auth />
+      }
+
     </div>
   )
 }
---
+++ client/src/pages/auth/auth.jsx
@@ -0,0 +1,17 @@
+import styles from './auth.module.css'
+
+
+
+function Auth() {
+    return(
+        <div>
+            <input type='email'></input>
+            <input type='password'></input>
+            <button>зарегестрироваться</button>
+        </div>
+    )
+}
+
+
+
+export default Auth
