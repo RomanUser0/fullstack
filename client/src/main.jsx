ff7deba628321b36ae22852fd4ffd97c465d6c39
roman <zroman938@gmail.com>
2024-03-10 20:33:12 +0300

    проверка

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import store from './store/index.js'
+import { store } from './store/index.js'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
