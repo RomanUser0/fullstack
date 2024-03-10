b11505e945be7bfa9298e09d3f7206dadb59b399
roman <zroman938@gmail.com>
2024-03-10 20:42:55 +0300

    проверка

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,13 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/users/users';
 import { Repository } from 'typeorm';
 import { AuthPayloadDto } from '../dtos/auth.dto';
-import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
 export class authService {
 
-    constructor(@InjectRepository(User) private userRepository: Repository<User>,
-        jwtService: JwtService) { }
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
 
 
 
