4963c6dc451b432aeda7149a1cb062adc4a5b8f1
roman <zroman938@gmail.com>
2024-03-10 21:08:56 +0300

    проверка

--- client/src/store/authApi.js
+++ client/src/store/authApi.js
@@ -3,16 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'api'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'api' }),
     endpoints: (build) => ({
-        auth: build.mutation({
-            query: (body) => ({
-                url: 'login',
-                method: 'POST',
-                body
-            }),
+        getUsers: build.query({
+            query: () => 'users'
+        }),
+            auth: build.mutation({
+                query: (body) => ({
+                    url: 'login',
+                    method: 'POST',
+                    body
+                }),
+            })
         })
     })
-})
 
-export const { useAuthMutation } = authApi
+export const {useAuthMutation, useGetUsersQuery} = authApi
