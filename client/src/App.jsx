b7087f056dc5e6d4498ff82d15e026b08dd804de
roman <zroman938@gmail.com>
2024-03-10 20:31:53 +0300

    проверка

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,12 @@ import Content from './components/content/content'
 import Header from './components/header/header'
 import Navigation from './components/navigation/navigation'
 import Auth from './pages/auth/auth'
-import { useSelector } from 'react-redux'
+
 
 
 function App() {
 
- const auth = useSelector(state => state.auth.auth)
- console.log(auth)
-
+ const [auth, setAuth] = useState(false)
 
   return (
     <div className={styles.appWrapper}>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authApi } from './authApi'
 
-
-export default configureStore({
+export const store = configureStore({
     reducer: {
-       
-    }
+       [authApi.reducerPath]: authApi.reducer ,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
 })
