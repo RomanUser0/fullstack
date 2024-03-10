ef4a17412b3c1b16eb0a72ee363535e478837264
roman <zroman938@gmail.com>
2024-03-10 20:49:59 +0300

    проверка

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { authController } from './controller/auth.controller';
 import { authService } from './service/auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/users/users';
 
 
 @Module({
-  imports: [JwtModule.register({
+  imports: [TypeOrmModule.forFeature([User]),JwtModule.register({
     secret: 'abc123',
     signOptions: {expiresIn: '1h'},
   })],
   controllers: [authController],
-  providers: [authService]
+  providers: [authService],
+  exports: [TypeOrmModule.forFeature([User])],
 })
 export class AuthModule {}
