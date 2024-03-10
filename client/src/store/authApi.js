11346d96abc42297b429872b639e5a2a6eef61b6
roman <zroman938@gmail.com>
2024-03-10 20:57:21 +0300

    проверка

--- client/src/store/authApi.js
+++ client/src/store/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'api'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'www.grifin.duckdns.org/api'}),
     endpoints: (build) => ({
         auth: build.mutation({
             query: (body) => ({
