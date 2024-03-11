909e9d9cbe2a597f4e7ece4c1d5fd0d713dfddfb
roman <zroman938@gmail.com>
2024-03-11 16:37:49 +0300

    проверка

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,19 +3,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/users/users';
 import { Repository } from 'typeorm';
 import { AuthPayloadDto } from '../dtos/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class authService {
 
-    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+    constructor(@InjectRepository(User) private userRepository: Repository<User>,
+        private jwtService: JwtService) { }
 
 
 
-   async validateUser ({ username, password }: AuthPayloadDto) {
-        const findUser = await this.userRepository.findOneBy({username, password})
-        console.log(findUser)
-        return findUser
-        
+    async validateUser({ username, password }: AuthPayloadDto) {
+        const findUser = await this.userRepository.findOneBy({ username, password })
+        if (!findUser) return null
+
+        if (password === findUser.password) {
+            const {password, ...user} = findUser
+           return this.jwtService.sign(user)
+        }
+
     }
 }
